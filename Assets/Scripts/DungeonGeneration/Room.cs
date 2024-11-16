391f57ec3103c07c6640cb185b84dd04b5b10803
Devream <devream24@gmail.com>
2024-11-16 12:37:30 +0100

    Procedural Generation

---
+++ Assets/Scripts/DungeonGeneration/Room.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+
+    public int Width;
+    public int Height;
+    public int X;
+    public int Y;
+
+    private void Start()
+    {
+        if(RoomController.instance == null)
+        {
+            Debug.Log("You pressed play in the wrong scene!");
+            return;
+        }
+
+        RoomController.instance.RegisterRoom(this);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(Width, Height, 0));
+    }
+
+    public Vector3 GetRoomCentre()
+    {
+        return new Vector3(X * Width, Y * Height);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            RoomController.instance.OnPlayerEnterRoom(this);
+        }
+    }
+
+
+}
